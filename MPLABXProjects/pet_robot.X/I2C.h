f752307e3ad14406026caebd1b1fa812bb8b5d2e
Ethan921001 <king6969003@gmail.com>
2024-12-09 19:25:59 +0800

    I2C and OLED functions

---
+++ MPLABXProjects/pet_robot.X/I2C.h
@@ -0,0 +1,17 @@
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+#include<pic18f4520.h>
+
+#define _XTAL_FREQ 1000000 
+// TODO Insert appropriate #include <>
+void I2C_Master_Init(unsigned long clock);
+
+void I2C_Start();
+
+void I2C_Stop();
+
+void I2C_Write(unsigned char data);
+
+
+
+
