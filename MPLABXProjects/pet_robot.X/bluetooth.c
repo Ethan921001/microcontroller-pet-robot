fbc4a4db032abc797ed475bc1bf8058ec1ae0511
Ethan921001 <king6969003@gmail.com>
2025-01-12 14:44:04 +0800

    add file bluetooth.c bluetooth.h

--- MPLABXProjects/pet_robot.X/bluetooth.c
+++ MPLABXProjects/pet_robot.X/bluetooth.c
@@ -1,13 +1,48 @@
-
 #include "bluetooth.h"
 
 
 void UART_Init(void) {
-    TRISC6 = 0;  // TX ???????
+    TRISC6 = 1;  // TX ???????
     TRISC7 = 1;  // RX ???????
-    SPBRG = 25;  // ??? 9600??? FOSC=1MHz
-    TXSTAbits.BRGH = 0;  // ???????
+    
+    
     RCSTAbits.SPEN = 1;  // ????
     TXSTAbits.TXEN = 1;  // ????
     RCSTAbits.CREN = 1;  // ????
+    PIR1bits.TXIF = 1;
+    PIR1bits.RCIF = 1;
+    
+    TXSTAbits.SYNC = 0;   
+    TXSTAbits.BRGH = 1;  // ???????
+    BAUDCONbits.BRG16 = 1;  
+    SPBRG = 25;  // ??? 9600??? FOSC=1MHz
+ 
+}
+
+// ????
+void UART_Write(char data) {
+    while (!TXSTAbits.TRMT);  // ?????????
+    TXREG = data;
+}
+
+// ????
+char UART_Read(void) {
+    while (!PIR1bits.RCIF);  // ????????
+    
+    if (RCREG == '\r')
+            UART_Write('\n');
+    
+    if('0'<=RCREG && RCREG<='9'){
+        LATB = RCREG-48;
+        //LATC = RCREG;
+    }
+    
+    if(RCREG == 'a'){
+        LATB = 0;
+    }
+    else if(RCREG == 'b'){
+        LATB = 7;
+    }
+    UART_Write(RCREG);
+    return RCREG;
 }
--- MPLABXProjects/pet_robot.X/bluetooth.h
+++ MPLABXProjects/pet_robot.X/bluetooth.h
@@ -1,2 +1,15 @@
 #include <pic18f4520.h>
 #include <xc.h>
+
+#ifndef _XTAL_FREQ
+#define _XTAL_FREQ 1000000    // ???????? 1 MHz
+#endif
+
+
+void UART_Init(void);
+
+// ????
+void UART_Write(char data);
+
+// ????
+char UART_Read(void);
